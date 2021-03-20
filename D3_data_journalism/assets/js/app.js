5023915e413fd29438d207ca5c25e8fecc53242c
Keiko Yara <Keiko@Keikos-MBP.fios-router.home>
2021-03-20 16:56:52 -0400

    changes

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,23 @@
 // @TODO: YOUR CODE HERE!
+// set svg size & margin
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+    top: 20,
+    right: 40,
+    botton: 60,
+    left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// create svg wrapper
+var svg = d3.select("#scatter")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+    attr("transform", `translate(${margin.left}, ${margin.top})`);
